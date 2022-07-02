f0092eeef95afca0a052623fea00dddf71eec8fd
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-02 16:49:45 -0300

    FEAT: add productsRout into index

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const app = require('./app');
+const productsRoute = require('./routes/productsRoute');
 require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+
+app.use('/products', productsRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
