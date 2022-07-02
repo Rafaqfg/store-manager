9f4734d7a634975106571c141d7f6557e651df6f
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-02 18:27:08 -0300

    FIX: typo

--- models/db.js
+++ models/db.js
@@ -8,4 +8,4 @@ const db = mysql.createPool({
   database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
-  module.esports = db;
+  module.exports = db;
--- models/productsModel.js
+++ models/productsModel.js
@@ -2,13 +2,13 @@ const db = require('./db');
 
 const productsModel = {
   async listAllProducts() {
-    const select = 'SELECT * FROM PRODUCTS';
+    const select = 'SELECT * FROM products';
     const [products] = await db.query(select);
     return products;
   },
 
   async listIdProduct(ID) {
-    const select = 'SELECT * FROM PRODUCTS WHERE ID = ?';
+    const select = 'SELECT * FROM products WHERE ID = ?';
     const [product] = await db.query(select, ID);
     return product;
   },
