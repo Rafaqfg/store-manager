bd35b8f059171a38d622065fdd8b7168748b29b2
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-05 14:31:26 -0300

    FIX: productsController

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,17 +1,17 @@
 const httpStatus = require('../utils/httpStatus');
-const { listAllProducts, listIdProduct } = require('../services/productsService');
+const productsService = require('../services/productsService');
 
 const productsControllers = {
   /** @type {import('express').RequestHandler} */
 
   async listAllProducts(_req, res) {
-    const products = await listAllProducts();
+    const products = await productsService.listAllProducts();
     res.status(httpStatus.OK).json(products);
   },
 
   async listIdProduct(req, res) {
     const { id } = req.params;
-    const [product] = await listIdProduct(id);
+    const [product] = await productsService.listIdProduct(id);
     if (!product) {
       return res.status(httpStatus.NOT_FOUND)
       .json({ message: 'Product not found' }); 
