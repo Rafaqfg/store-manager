e9d71694d65ab73c74e34303202e52bd033985f4
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-05 14:51:20 -0300

    FEAT:  add test coverage to productsService

---
+++ tests/unit/services/productsService.test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiAsPromised = require('chai-as-promised');
+const productsModel = require('../../../models/productsModel');
+const productsService = require('../../../services/productsService');
+
+chai.use(chaiAsPromised);
+
+describe('services/productsService', () => {
+  beforeEach(sinon.restore);
+
+  describe('Test listAllProducts function', () => {
+
+    it('should throw error if productModel.listAllProducts throw error', () => {
+      sinon.stub(productsModel, 'listAllProducts').rejects();
+      return chai.expect(productsService.listAllProducts()).to.eventually.be.rejected;
+    })
+
+    it('should resolves if productModel.listAllProducts resolves', () => {
+      sinon.stub(productsModel, 'listAllProducts').resolves();
+      return chai.expect(productsService.listAllProducts()).to.eventually.be.true;
+    })
+  })
+
+  describe('Test listIdProduct function', () => {
+
+    it('should throw error if productModel.listIdProduct throw error', () => {
+      sinon.stub(productsModel, 'listIdProduct').rejects();
+      return chai.expect(productsService.listIdProduct()).to.eventually.be.rejected;
+    })
+
+    it('should resolves if productModel.listIdProduct resolves', () => {
+      sinon.stub(productsModel, 'listIdProduct').resolves();
+      return chai.expect(productsService.listIdProduct()).to.eventually.be.true;
+    })
+  })
+})
