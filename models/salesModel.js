20c9cc2c1f52cf2ed6cc0671ccddcb90d1f33ed1
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-12 15:04:52 -0300

    FEAT: create salesModel file

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,1 +1,17 @@
 const db = require('./db');
+
+const salesModel = {
+
+  async createSale(saleId, productId, quantity) {
+    const insert = 'INSERT INTO sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?);';
+    await db.query(insert, [saleId, productId, quantity])
+  },
+
+  async addSaleId() {
+    const insert = 'INSERT INTO sales (date) VALUES (now());';
+    const [{ insertId }] = await db.query(insert);
+    return insertId;
+  },
+};
+
+module.exports = salesModel;
