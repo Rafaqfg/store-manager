68254fac0f4e763b8ae1eedf6b15db284ddf6033
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-12 15:05:44 -0300

    FEAT: configure salesControllers file

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -1,1 +1,18 @@
 const httpStatus = require('../utils/httpStatus');
+const salesServices = require('../services/salesServices');
+
+const salesControllers = {
+  /** @type {import('express').RequestHandler} */
+
+  async createSale(req, res) {
+    const sale = req.body;
+    sale.forEach((item) => {
+      salesServices.validateSale(item);
+    });
+    const newSale = await salesServices.createSale(sale);
+    res.status(httpStatus.CREATED).json(newSale);
+  }
+
+}
+
+module.exports = salesControllers;
