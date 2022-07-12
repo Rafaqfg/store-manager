52317509689bc3fe86c4440917157c13d3e89a1f
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-12 15:06:58 -0300

    FEAT: add salesRoute into index file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const app = require('./app');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const productsRoute = require('./routes/productsRoute');
+const salesRoute = require('./routes/salesRoute');
 require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.use('/products', productsRoute);
+app.use('/sales', salesRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
