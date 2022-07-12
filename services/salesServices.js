618d6d014e7c7f8e71c4fa13a57617ae040ab651
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-12 15:05:23 -0300

    FEAT: configure salesServices file

--- services/salesServices.js
+++ services/salesServices.js
@@ -1,2 +1,24 @@
 const Joi = require('joi');
 const { runSchema } = require('../utils/validators');
+const salesModel = require('../models/salesModel');
+
+const salesServices = {
+
+  validateSale: runSchema(Joi.object({
+    productId: Joi.number().required(),
+    quantity: Joi.number().min(1).required(),
+  })),
+
+  async createSale(sale) {
+    const saleId = await salesModel.addSaleId();
+    await Promise.all(sale.map((product) =>
+      salesModel.createSale(saleId, product.productId, product.quantity)));
+    const newSale = {
+      id: saleId,
+      itemsSold: sale,
+    };
+    return newSale;
+  },
+};
+
+module.exports = salesServices;
