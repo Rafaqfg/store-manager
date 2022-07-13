1e30a25ea81405abbbc3b266508b79441fa277a5
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-12 21:44:26 -0300

    FEAT: create '/sale' and '/sale:id' routes

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -3,6 +3,8 @@ const salesControllers = require('../controllers/salesControllers');
 
 const salesRoute = Router();
 
+salesRoute.get('/:id', salesControllers.getSaleById)
 salesRoute.post('/', salesControllers.createSale);
+salesRoute.get('/', salesControllers.getAllSalesList);
 
 module.exports = salesRoute;
