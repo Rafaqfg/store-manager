ce3291724f7ede5941fb97d8feeab2ba7675c897
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-14 18:39:38 -0300

    TEST: increase salesControllers coverage

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -4,17 +4,6 @@ const salesServices = require('../services/salesServices');
 const salesControllers = {
   /** @type {import('express').RequestHandler} */
 
-  async createSale(req, res) {
-    const sale = req.body;
-    await salesServices.saleIsValid(sale);
-    const validateId = (await salesServices.validateId(sale))
-      .every((item) => (item.length > 0));
-    if (!validateId) throw new Error('Product not found');
-    
-    const newSale = await salesServices.createSale(sale);
-    res.status(httpStatus.CREATED).json(newSale);
-  },
-
   async getAllSalesList(_req, res) {
     const salesList = await salesServices.getAllSalesList();
     res.status(httpStatus.OK).json(salesList);
@@ -27,6 +16,17 @@ const salesControllers = {
       return res.status(httpStatus.NOT_FOUND).json({ message: 'Sale not found' });
     }
     res.status(httpStatus.OK).json(sale);
+  },
+
+  async createSale(req, res) {
+    const sale = req.body;
+    await salesServices.saleIsValid(sale);
+    const isValid = (await salesServices.validateId(sale))
+      .every((item) => (item.length > 0));
+    if (!isValid) throw new Error('Product not found');
+    
+    const newSale = await salesServices.createSale(sale);
+    res.status(httpStatus.CREATED).json(newSale);
   },
 };
 
--- tests/unit/controllers/salesControllers.test.js
+++ tests/unit/controllers/salesControllers.test.js
@@ -2,21 +2,21 @@ const chai = require('chai');
 const sinon = require('sinon');
 const chaiAsPromised = require('chai-as-promised');
 const salesControllers = require('../../../controllers/salesControllers');
-const salesServices = require('../../../services/salesServices')
+const salesServices = require('../../../services/salesServices');
 
 chai.use(chaiAsPromised);
 
 describe('controllers/salesControllers', () => {
   beforeEach(sinon.restore);
 
-  describe('Test getAllSalesList function', () => {
+  describe('1. Test getAllSalesList function', () => {
 
-    it('should throw error if salesServices.getAllSalesList throw error', () => {
+    it('1.1 should throw error if salesServices.getAllSalesList throw error', () => {
       sinon.stub(salesServices, 'getAllSalesList').rejects();
       return chai.expect(salesControllers.getAllSalesList()).to.eventually.be.rejected;
     })
 
-    it('should call res method and return an array if salesServices.getAllSalesList resolves', async () => {
+    it('1.2 should call res method and return an array if salesServices.getAllSalesList resolves', async () => {
       sinon.stub(salesServices, 'getAllSalesList').resolves([]);
       const res = {
         status: sinon.stub().callsFake(() => res),
@@ -28,14 +28,14 @@ describe('controllers/salesControllers', () => {
     })
   })
 
-  describe('Test getSaleById function', () => {
+  describe('2. Test getSaleById function', () => {
 
-    it('should throw error if salesServices.getSaleById(id) throw error', () => {
+    it('2.1 should throw error if salesServices.getSaleById(id) throw error', () => {
       sinon.stub(salesServices, 'getSaleById').rejects();
       return chai.expect(salesControllers.getSaleById(1)).to.eventually.be.rejected;
     });
 
-    it('should return status 404 and message "Sale not found" if salesServices.getSaleById(id) dont return anything', async () => {
+    it('2.2 should return status 404 and message "Sale not found" if salesServices.getSaleById(id) dont return anything', async () => {
       const req = { params: { id: 1 } };
       const res = {
         status: sinon.stub().callsFake(() => res),
@@ -49,7 +49,7 @@ describe('controllers/salesControllers', () => {
       });
     });
 
-    it('should return status 200 and an array if salesServices.getSaleById(id) resolves', async () => {
+    it('2.3 should return status 200 and an array if salesServices.getSaleById(id) resolves', async () => {
       const list = [];
       const req = { params: { id: 1 } };
       const res = {
@@ -63,22 +63,52 @@ describe('controllers/salesControllers', () => {
     });
   })
 
-  describe('Test addProduct function', () => {
+  describe('3. Test createSale function', () => {
 
-    it('should throw error if salesServices.saleIsValid throw error', () => {
-      sinon.stub(salesServices, 'saleIsValid').rejects();
-      return chai.expect(salesControllers.getSaleById([])).to.eventually.be.rejected;
+    it('3.1 should throw error if salesServices.saleIsValid throw error', () => {
+      const sale = [];
+      sinon.stub(salesServices, 'saleIsValid').rejects(sale);
+      return chai.expect(salesControllers.createSale([])).to.eventually.be.rejected;
     })
-    it('should throw error if salesServices.addProduct throw error', () => {
-
-    })
-    it('should throw error if salesServices.addProduct throw error', () => {
-
-    })
-
-    it('should call res method and return an object if salesServices.listAllProducts resolves', async () => {
+    it('3.2 should throw error if salesServices.validateId throw error', () => {
+      const sale = [];
+      sinon.stub(salesServices, 'saleIsValid').resolves(sale);
+      sinon.stub(salesServices, 'validateId').rejects(sale);
+      return chai.expect(salesControllers.createSale([])).to.eventually.be.rejected;
 
     })
+    it('3.3 should throw error if salesServices.validateId return an empty array', async () => {
+      // const error = Error('Product not found');
+      // const emptyList = [];
+      // const req = {
+      //   body: [{
+      //     "productId": 8,
+      //     "quantity": 1
+      //   }]
+      // };
+      // const res = {};
+      // const create = await salesControllers.createSale(req, res);
+      // sinon.stub(salesServices, 'saleIsValid').returns(true);
+      // sinon.stub(salesServices, 'validateId').returns(emptyList);
+      // // sinon.stub(salesControllers, 'createSale').throw(Error, 'Product not found');
+      // return chai.expect(salesControllers.createSale.bind(salesControllers, 'createSale')).to.throw(new Error('Product not found'));
+    })
 
+    it('3.4 should call res method and return an object if salesServices.createSale resolves', async () => {
+      const obj = {};
+      const req = {
+        body: [{
+          "productId": 1,
+          "quantity": 1
+        }] };
+      const res = {
+        status: sinon.stub().callsFake(() => res),
+        json: sinon.stub().returns(),
+      };
+      sinon.stub(salesServices, 'createSale').resolves(obj);
+      await salesControllers.createSale(req, res);
+      chai.expect(res.status.getCall(0).args[0]).to.equal(201);
+      chai.expect(res.json.getCall(0).args[0]).to.deep.equal(obj);
+    })
   })
 });
