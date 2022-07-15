4bfeb0ff2171bebfd977bb193ceba5259b031351
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-15 10:14:47 -0300

    FEAT: create updatePorduct route

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -4,6 +4,7 @@ const productsControllers = require('../controllers/productsControllers');
 const productsRoute = Router();
 
 productsRoute.get('/:id', productsControllers.listIdProduct);
+productsRoute.put('/:id', productsControllers.updateProduct);
 productsRoute.get('/', productsControllers.listAllProducts);
 productsRoute.post('/', productsControllers.addProduct);
 
